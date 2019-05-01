1239eb06d3b323142ba1738ce0077ae568b95924
Mohd <fmohd195@gmail.com>
2019-05-01 19:37:09 +0530

    shallow test -1

--- src/Components/__tests__/App.test.js
+++ src/Components/__tests__/App.test.js
@@ -1,10 +1,22 @@
 import App from '../App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import CommentBox from '../CommentBox';
+import {shallow} from 'enzyme';
 
 it('shows a comment box', ()=>{
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    expect(div.innerHTML).toContain('Comment Box');
-    ReactDOM.unmountComponentAtNode(div);
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+    // **find() - will return all the instances that were found of the commentbox component in an array.
+    
 })
+
+
+
+
+// it('shows a comment box', ()=>{
+//     const div = document.createElement('div');
+//     ReactDOM.render(<App />, div);
+//     expect(div.innerHTML).toContain('Comment Box');
+//     ReactDOM.unmountComponentAtNode(div);
+// })
